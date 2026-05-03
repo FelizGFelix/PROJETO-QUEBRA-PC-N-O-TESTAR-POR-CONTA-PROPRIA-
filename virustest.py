766ffdaadb1499024b0f319d929923e3b50e4023
Felipe Felix <lipegfelix@gmail.com>
2026-05-03 04:01:12 -0300

    versão atualizada

--- virustest.py
+++ virustest.py
@@ -1,13 +1,10 @@
 from tkinter import *
 
-
 while True:
     janela = Tk()
     img = PhotoImage(file="virus/Trollface.png")
     Label(janela, image=img).pack()
     janela.title("XD LOL TROLL")
     janela.geometry("500x300")
+    janela.mainloop()
 
-
-#apenas para testes do código para parar quando necessário:
-#janela.mainloop()
